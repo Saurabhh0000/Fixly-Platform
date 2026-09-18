691b4fe8842c3d4bd0777852bd54e1c35e33bdf1
Saurabh Kumar <158316898+Saurabhh0000@users.noreply.github.com>
2026-09-18 17:13:26 +0530

    Add profile and address CRUD security

--- Fixly-Backend/src/main/java/com/fixly/service/AddressService.java
+++ Fixly-Backend/src/main/java/com/fixly/service/AddressService.java
@@ -6,10 +6,14 @@ import com.fixly.dto.request.AddressRequest;
 import com.fixly.dto.response.AddressResponse;
 
 public interface AddressService {
-	
-	AddressResponse addAddress(Long id, AddressRequest request);
-	List<AddressResponse> getUserAddress(Long id);
-	List<String> getAllCities();
-	
 
+    AddressResponse addAddress(Long id, AddressRequest request, String authenticatedEmail);
+
+    List<AddressResponse> getUserAddress(Long id, String authenticatedEmail);
+
+    AddressResponse updateAddress(Long addressId, AddressRequest request, String authenticatedEmail);
+
+    void deleteAddress(Long addressId, String authenticatedEmail);
+
+    List<String> getAllCities();
 }
