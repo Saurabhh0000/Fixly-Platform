22932eab959e1482218f1c477f7d4b7c13029057
Saurabh Kumar <158316898+Saurabhh0000@users.noreply.github.com>
2026-09-19 11:36:59 +0530

    Add contact history service contract

--- Fixly-Backend/src/main/java/com/fixly/service/ContactService.java
+++ Fixly-Backend/src/main/java/com/fixly/service/ContactService.java
@@ -2,7 +2,13 @@ package com.fixly.service;
 
 import com.fixly.dto.request.ContactRequest;
 import com.fixly.dto.response.ContactResponse;
+import com.fixly.dto.response.ContactAdminResponse;
+import com.fixly.dto.response.PageResponse;
+import org.springframework.data.domain.Pageable;
+import com.fixly.enums.ContactStatus;
 
 public interface ContactService {
     ContactResponse submitContactMessage(ContactRequest request);
+
+    PageResponse<ContactAdminResponse> getMyContactHistory(ContactStatus status, String search, Pageable pageable);
 }
